8c7b7f2a84cc14679b05c89463d9549fdde5a59c
qdimanche <73292719+qdimanche@users.noreply.github.com>
2024-04-05 16:28:06 +0200

    Add Movie Model
    Title, Year, Genre

--- streaming_app/models.py
+++ streaming_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Movie(models.Model):
+    title = models.CharField(max_length=30)
+    year = models.IntegerField(null=True, blank=True)
+    genre = models.CharField(max_length=200)
