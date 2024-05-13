cb4d2f42e78fca83e6f919113200489d68a9660b
AntSch <antoine97410@hotmail.fr>
2024-05-13 22:47:27 +0200

    Make a basic Movie model

--- streaming_app/models.py
+++ streaming_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Movie(models.Model):
+    title = models.CharField(max_length=200)
+    year = models.CharField(max_length=4)
+    type = models.CharField(max_length=20)
+    poster = models.URLField()
+
+    def __str__(self):
+        return self.title
