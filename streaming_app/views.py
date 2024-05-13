1b2d2358ae274f16895b44618f8d4931719e5d9c
AntSch <antoine97410@hotmail.fr>
2024-05-13 22:47:52 +0200

    * Setup MovieListView and AddMovieView views
    * Add corresponding template index.html and add_movie.html
    
    * Add a get_api_key method to not share the api key on git

--- streaming_app/views.py
+++ streaming_app/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+import requests
+import os
 
-# Create your views here.
+from django.views.generic import ListView
+
+from streaming_app.models import Movie
+
+
+def get_api_key():
+    api_key_path = os.path.join(os.getcwd(), 'API_KEY')
+    with open(api_key_path, 'r') as file:
+        api_key = file.read().strip()
+    return api_key
+
+
+class MovieListView(ListView):
+    template_name = 'index.html'
+    context_object_name = 'movies'
+
+    def get_queryset(self):
+        return Movie.objects.all()
+
+
+class AddMovieView(ListView):
+    template_name = 'add_movie.html'
+    context_object_name = 'movies'
+
+    def get_queryset(self):
+        api_key = get_api_key()
+        search_query = 'Harry'
+        url = f'https://www.omdbapi.com/?apikey={api_key}&s={search_query}'
+        response = requests.get(url)
+        data = response.json()
+        return data.get('Search', [])
