b48d47b3a723f0d50cb4e773530bc774bc7a9256
AntSch <antoine97410@hotmail.fr>
2024-05-14 21:58:16 +0200

    * Search in the database before querying the API
    * Add movie.source to identify the origin of a movie card
    
    * Save multiple api results instead of one

--- streaming_app/views.py
+++ streaming_app/views.py
@@ -13,6 +13,49 @@ def get_api_key():
     return api_key
 
 
+def get_local_movies(search_query):
+    return Movie.objects.filter(title__icontains=search_query)
+
+
+def fetch_movies_from_api(api_key, search_query):
+    url = f'https://www.omdbapi.com/?apikey={api_key}&s={search_query}&type=movie'
+    response = requests.get(url)
+    data = response.json()
+
+    if 'Response' in data and data['Response'] == 'True':
+        movies_list = []
+        for item in data.get('Search', []):
+            imdb_id = item.get('imdbID')
+            movie, created = Movie.objects.get_or_create(
+                omdb_id=imdb_id,
+                defaults={
+                    'title': item.get('Title', ''),
+                    'year': item.get('Year', ''),
+                    'poster': item.get('Poster',
+                                       'https://downtownwinnipegbiz.com/wp-content/uploads/2020/02'
+                                       '/placeholder-image.jpg'),
+                }
+            )
+
+            if created:
+                detail_url = f'https://www.omdbapi.com/?apikey={api_key}&i={imdb_id}'
+                detail_response = requests.get(detail_url)
+                detail_data = detail_response.json()
+
+                genres = detail_data.get('Genre', '').split(', ')
+                for genre_name in genres:
+                    genre, _ = Genre.objects.get_or_create(name=genre_name.strip())
+                    movie.genres.add(genre)
+                movie.save()
+                movie.source = "api"
+                print(f"Movie {movie.title} has been created.")
+            movies_list.append(movie)
+        return movies_list
+    else:
+        print("No results found with this search arguments")
+        return []
+
+
 class MovieListView(ListView):
     template_name = 'index.html'
     context_object_name = 'movies'
@@ -20,39 +63,16 @@ class MovieListView(ListView):
     def get_queryset(self):
         search_query = self.request.GET.get('search_query')
         if search_query:
+            local_movies = get_local_movies(search_query)
+            if local_movies.exists():
+                for movie in local_movies:
+                    movie.source = "database"
+                return list(local_movies)
+
             api_key = get_api_key()
-            url = f'https://www.omdbapi.com/?apikey={api_key}&t={search_query}&type=movie'
-            response = requests.get(url)
-            data = response.json()
-
-            if 'Response' in data and data['Response'] == 'True':
-                movie_data = data
-                movie_data['omdb_id'] = movie_data['imdbID']
-
-                movie, created = Movie.objects.get_or_create(
-                    omdb_id=movie_data['omdb_id'],
-                    defaults={
-                        'title': movie_data.get('Title', ''),
-                        'year': movie_data.get('Year', ''),
-                        'poster': movie_data.get('Poster', ''),
-                    }
-                )
-
-                if created:
-                    genres = movie_data.get('Genre', '').split(', ')
-
-                    for genre_name in genres:
-                        genre, created = Genre.objects.get_or_create(
-                            name=genre_name
-                        )
-                        movie.genres.add(genre)
-
-                    movie.save()
-                    print(f"Movie {movie.title} has been created.")
-
-                return Movie.objects.filter(omdb_id=movie_data['omdb_id'])
-            else:
-                print("No results founded with this search arguments")
-                return Movie.objects.none()
+            return fetch_movies_from_api(api_key, search_query)
         else:
-            return Movie.objects.all().order_by('-id')
+            movies = Movie.objects.all().order_by('-id')
+            for movie in movies:
+                movie.source = "database"
+            return list(movies)
