6933b306b3498b61a3b5828e0b35635944e730c4
AntSch <antoine97410@hotmail.fr>
2024-05-14 22:47:26 +0200

    Add Genre model to admin.py

--- streaming_app/admin.py
+++ streaming_app/admin.py
@@ -1,6 +1,5 @@
-# Register your models here.
 from django.contrib import admin
+from .models import Movie, Genre
 
-from .models import Movie
-
+admin.site.register(Genre)
 admin.site.register(Movie)
