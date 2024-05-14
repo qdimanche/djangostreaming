2d1077a4b0235c3b8b07399a0166f0b12dba88d1
qdimanche <73292719+qdimanche@users.noreply.github.com>
2024-05-14 13:44:19 +0200

    Update Movie Model, Views

--- streaming_app/models.py
+++ streaming_app/models.py
@@ -1,12 +1,20 @@
 from django.db import models
 
 
+class Genre(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Movie(models.Model):
-    omdb_id = models.CharField(max_length=50, unique=True, default='')
+    omdb_id = models.CharField(max_length=50, unique=True)
     title = models.CharField(max_length=255)
     year = models.CharField(max_length=4)
-    type = models.CharField(max_length=255)
-    poster = models.URLField()
+    genres = models.ManyToManyField(Genre)
+    type = models.CharField(max_length=255, default='movie')
+    poster = models.URLField(default="https://downtownwinnipegbiz.com/wp-content/uploads/2020/02/placeholder-image.jpg")
 
     def __str__(self):
         return self.title
--- streaming_app/urls.py
+++ streaming_app/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
-from .views import MovieListView, AddMovieView, AddToLibraryView
+from .views import MovieListView
 
 app_name = "streaming_app"
 urlpatterns = [
     path('', MovieListView.as_view(), name='index'),
-    path('add_movie/', AddMovieView.as_view(), name='add_movie'),
-    path('add_to_library/<str:movie_id>/', AddToLibraryView.as_view(), name='add_to_library'),
 ]
