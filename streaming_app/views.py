054be45d73975ca992f038ab2770785b60aa4c2a
qdimanche <73292719+qdimanche@users.noreply.github.com>
2024-05-14 15:42:34 +0200

    Update Movie View to add Genre

--- streaming_app/views.py
+++ streaming_app/views.py
@@ -21,35 +21,38 @@ class MovieListView(ListView):
         search_query = self.request.GET.get('search_query')
         if search_query:
             api_key = get_api_key()
-            url = f'https://www.omdbapi.com/?apikey={api_key}&s={search_query}'
+            url = f'https://www.omdbapi.com/?apikey={api_key}&t={search_query}&type=movie'
             response = requests.get(url)
             data = response.json()
 
-            for movie_data in data.get('Search', []):
+            if 'Response' in data and data['Response'] == 'True':
+                movie_data = data
                 movie_data['omdb_id'] = movie_data['imdbID']
 
                 movie, created = Movie.objects.get_or_create(
                     omdb_id=movie_data['omdb_id'],
                     defaults={
-                        'title': movie_data['Title'],
-                        'year': movie_data['Year'],
-                        'poster': movie_data['Poster']
+                        'title': movie_data.get('Title', ''),
+                        'year': movie_data.get('Year', ''),
+                        'poster': movie_data.get('Poster', ''),
                     }
                 )
 
                 if created:
-                    genres = movie_data['Genre'].split(', ')
+                    genres = movie_data.get('Genre', '').split(', ')
 
                     for genre_name in genres:
                         genre, created = Genre.objects.get_or_create(
                             name=genre_name
                         )
                         movie.genres.add(genre)
+
                     movie.save()
                     print(f"Movie {movie.title} has been created.")
-                else:
-                    print(f"Movie {movie.title} already exists in the database.")
 
-            return Movie.objects.filter(omdb_id__in=[movie['omdb_id'] for movie in data.get('Search', [])])
+                return Movie.objects.filter(omdb_id=movie_data['omdb_id'])
+            else:
+                print("No results founded with this search arguments")
+                return Movie.objects.none()
         else:
             return Movie.objects.all().order_by('-id')
