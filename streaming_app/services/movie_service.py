1f15ff4068340b90dbd6127bb947426d2ff27596
qdimanche <73292719+qdimanche@users.noreply.github.com>
2024-05-15 10:14:57 +0200

    Code refactor in movie_service.py

--- streaming_app/services/movie_service.py
+++ streaming_app/services/movie_service.py
@@ -8,44 +8,43 @@ def get_local_movies(search_query):
 
 
 def fetch_movies_from_api(api_key, search_query):
+    movies_list = []
     url = f'https://www.omdbapi.com/?apikey={api_key}&s={search_query}&type=movie'
     response = requests.get(url)
     data = response.json()
 
-    if 'Response' in data and data['Response'] == 'True':
-        movies_list = []
-        for item in data.get('Search', []):
-            imdb_id = item.get('imdbID')
-            movie, created = Movie.objects.get_or_create(
-                omdb_id=imdb_id,
-                defaults={
-                    'title': item.get('Title', ''),
-                    'year': item.get('Year', ''),
-                    'poster': item.get('Poster',
-                                       'https://downtownwinnipegbiz.com/wp-content/uploads/2020/02'
-                                       '/placeholder-image.jpg'),
-                }
-            )
+    if 'Response' not in data or data['Response'] != 'True':
+        return print("Error while fetching movies")
 
-            if created:
-                detail_url = f'https://www.omdbapi.com/?apikey={api_key}&i={imdb_id}'
-                detail_response = requests.get(detail_url)
-                detail_data = detail_response.json()
+    for item in data.get('Search', []):
+        movie, created = Movie.objects.get_or_create(
+            omdb_id=item.get('imdbID'),
+            defaults={
+                'title': item.get('Title', ''),
+                'year': item.get('Year', ''),
+                'poster': item.get('Poster',
+                                   'https://downtownwinnipegbiz.com/wp-content/uploads/2020/02'
+                                   '/placeholder-image.jpg'),
+                'plot': item.get('Plot', ''),
+                'director': item.get('Director', ''),
+                'metascore': item.get('Metascore', ''),
+            }
+        )
 
-                movie.plot = detail_data.get('Plot', '')
-                movie.director = detail_data.get('Director', '')
-                movie.metascore = detail_data.get('Metascore', '')
+        if not created:
+            return print('Error while adding movie')
 
-                genres = detail_data.get('Genre', '').split(', ')
-                for genre_name in genres:
-                    genre, _ = Genre.objects.get_or_create(name=genre_name.strip())
-                    movie.genres.add(genre)
+        movie.source = 'api'
+        movie.save()
 
-                movie.save()
-                movie.source = "api"
-                print(f"Movie {movie.title} has been created.")
-            movies_list.append(movie)
-        return movies_list
-    else:
-        print("No results found with this search arguments")
-        return []
+        genres = item.get('Genre', '').split(', ')
+        for genre_name in genres:
+            genre, created = Genre.objects.get_or_create(name=genre_name.strip())
+
+            if not created:
+                print('Error while adding genres')
+
+            movie.genres.add(genre)
+
+        movies_list.append(movie)
+    return movies_list
