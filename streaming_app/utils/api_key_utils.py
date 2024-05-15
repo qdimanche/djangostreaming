4c3691e95dc0e43f456c5269a39b951092d3a30a
AntSch <antoine97410@hotmail.fr>
2024-05-15 09:50:38 +0200

    Add description for get_api_key

--- streaming_app/utils/api_key_utils.py
+++ streaming_app/utils/api_key_utils.py
@@ -2,6 +2,9 @@ import os
 
 
 def get_api_key():
+    """
+    Read the API key from the API_KEY file.
+    """
     api_key_path = os.path.join(os.getcwd(), 'API_KEY')
     with open(api_key_path, 'r') as file:
         api_key = file.read().strip()
